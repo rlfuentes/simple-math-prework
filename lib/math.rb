3cb51db390933a34596cf1ba8e51aede8338abb7
Robert <r_lee94@yahoo.com>
2019-02-07 20:33:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,25 @@
 def addition(num1, num2)
+  1+1
 end
 
 def subtraction(num1, num2)
+  8-6
 end
 
 def division(num1, num2)
+  18/9
 end
 
 def multiplication(num1, num2)
+  6*6
 end
 
 def modulo(num1, num2)
+  69%8
 end
 
 def square_root(num)
+  
 end
 
 def order_of_operation(num1, num2, num3, num4)
